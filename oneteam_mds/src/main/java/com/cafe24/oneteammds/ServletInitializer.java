2f27cdb2340bc7346878efb5f1921e3352f6df66
geungwan <kevinyook07@gmail.com>
2019-10-07 15:24:15 +0900

    1. oneteam_mds 프로젝트 생성
    2. git 설정 후 배포

---
+++ oneteam_mds/src/main/java/com/cafe24/oneteammds/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cafe24.oneteammds;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OneTeamMdsApplication.class);
+	}
+
+}
