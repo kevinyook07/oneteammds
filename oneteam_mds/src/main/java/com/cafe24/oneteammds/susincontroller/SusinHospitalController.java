d655281ae551a5c0afcdec0685d55166a303677e
yongsuLee <ud1746@naver.com>
2019-10-08 09:41:04 +0900

    이용수
    1. 주석처리

--- oneteam_mds/src/main/java/com/cafe24/oneteammds/susincontroller/SusinHospitalController.java
+++ oneteam_mds/src/main/java/com/cafe24/oneteammds/susincontroller/SusinHospitalController.java
@@ -3,13 +3,14 @@ package com.cafe24.oneteammds.susincontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller//어노테이션 controller
+@Controller//어노테이션 controller 설정
 public class SusinHospitalController {
 	
-	@GetMapping("/susinHospitalInsert")//어노테이션 getmapping // susinHospitalInsert 경로 설정
-	public String susinHospital() {
+	@GetMapping("/susinHospitalInsert")//어노테이션 getmapping 설정
+	//접근 지정자 public(제한없음)으로 설정 후 리턴 데이터 타입은 String 메서드명 susinHospital
+	public String susinHospital() {      
 		
-		return "/susinHospital/susinHospitalInsert/susinHospitalInsert";
+		return "/susinHospital/susinHospitalInsert/susinHospitalInsert"; //리턴 susinHospitalInsert.html  경로로 설정
 	}
 
 }
