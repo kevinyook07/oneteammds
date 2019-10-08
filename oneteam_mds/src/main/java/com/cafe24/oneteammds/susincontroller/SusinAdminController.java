a7e10d422642b654d6f74147a24f4041dc038908
geungwan <kevinyook07@gmail.com>
2019-10-08 09:38:52 +0900

    육근관
    1. susinAdmin → susinAdminInsert → susinAdminInsert.html 생성 및 주석처리
    
    2. susinAdmin → susinAdminList → susinAdminList.html 생성 및 주석처리
    
    3. susinAdmin → susinAdminYocheng → susinAdminYocheng.html 생성 및 주석처리
    
    4. susinAdmin → susinAdminYochengList → susinAdminYochengList.html 생성 및
    주석처리
    
    5. susinmapper → SusinAdminMapper.java 생성 및 주석처리
    
    6. susinmapper → SusinAdminMapper.xml 생성 및 주석처리
    
    7. susinservice → SusinAdminService.java 생성 및 주석처리

--- oneteam_mds/src/main/java/com/cafe24/oneteammds/susincontroller/SusinAdminController.java
+++ oneteam_mds/src/main/java/com/cafe24/oneteammds/susincontroller/SusinAdminController.java
@@ -1,16 +1,29 @@
 package com.cafe24.oneteammds.susincontroller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cafe24.oneteammds.susinservice.SusinAdminService;
+import com.cafe24.oneteammds.susinvo.SusinAdmin;
 
 @Controller //어노테이션 controller
 public class SusinAdminController {
 
+	@Autowired //어너테이션 autowired
+	private SusinAdminService susinAdminService;
 	
-	@GetMapping("/susinAdminInsert") //어노테이션 getmapping // susinAdminInsert 경로 설정
-	public String susinAdminInsert() {
+	
+	@GetMapping("/susinAdminYochengList") //어노테이션 getmapping // susinAdminInsert 경로 설정
+	public String susinAdminYochengList(SusinAdmin susinAdmin, Model model) {
 		
-		return "/susinAdmin/susinAdminInsert/susinAdminInsert";
+		List<SusinAdmin> list = susinAdminService.getsusinAdminYochengList();
+		model.addAttribute("susinAdminYochengList", list);
+		
+		return "/susinAdmin/susinAdminYochengList/susinAdminYochengList";
 	}
 	
 }
