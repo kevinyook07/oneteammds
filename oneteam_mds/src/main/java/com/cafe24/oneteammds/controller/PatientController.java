804ddd2c2b2180c117652dc7ad5247b364beac3e
goyeongmin <gmlwhd8848@naver.com>
2019-10-10 11:40:30 +0900

    고영민
    1. 주석처리 및 들여쓰기 수정

--- oneteam_mds/src/main/java/com/cafe24/oneteammds/controller/PatientController.java
+++ oneteam_mds/src/main/java/com/cafe24/oneteammds/controller/PatientController.java
@@ -1,4 +1,5 @@
 package com.cafe24.oneteammds.controller;
+
 //계정 오류로 인한 push
 import java.util.List;
 import java.util.Map;
@@ -15,122 +16,126 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.cafe24.oneteammds.service.PatientService;
 import com.cafe24.oneteammds.vo.Patient;
 
-
 @Controller
 public class PatientController {
-	
-	@Autowired PatientService patientService; 
-	
+
+	@Autowired
+	PatientService patientService;
+
+	// 환자 권한 로그인 폼
 	@GetMapping("patientLogin")
 	public String patientLogin() {
 		return "/login/patientLogin/patientLogin";
 	}
-	
+
+	// 환자 권한 로그인 및 유효성 검사
 	@PostMapping("/patientLogin")
 	public String patientLogin(Patient patient, HttpSession session, Model model) {
-		//입력된 아이디 비밀번호
+		// 입력된 아이디 비밀번호
 		System.out.println(patient.toString() + "<--입력된 정보");
-		
-		Map<String,Object> map = patientService.getPatientLogin(patient);
-		String result 		= (String) map.get("result"); 
-		Patient loginPatient 	= (Patient) map.get("loginPatient");
-		
-		//로그인 실패 화면 login
-		if(!result.equals("로그인 성공")) {
+
+		Map<String, Object> map = patientService.getPatientLogin(patient);
+		String result = (String) map.get("result");
+		Patient loginPatient = (Patient) map.get("loginPatient");
+
+		// 로그인 실패 화면 login
+		if (!result.equals("로그인 성공")) {
 			model.addAttribute("result", result);
 			return "/login/patientLogin/patientLogin";
 		}
-		session.setAttribute("SID"		, loginPatient.getP_id());
-		session.setAttribute("SLEVEL" , loginPatient.getP_level());
-		session.setAttribute("SNAME"	, loginPatient.getP_name());
-		
-		//로그인 성공 화면 index
+		session.setAttribute("SID", loginPatient.getP_id());
+		session.setAttribute("SLEVEL", loginPatient.getP_level());
+		session.setAttribute("SNAME", loginPatient.getP_name());
+
+		// 로그인 성공 화면 index
 		return "redirect:/";
 	}
-	
+
 	/*
 	 * @GetMapping("/logout") public String logout(HttpSession session) {
 	 * session.invalidate(); return "redirect:/"; }
 	 */
 
-	
-	
+	// 환자 권한 리스트
 	@GetMapping("/patientList")
 	public String getPatientList(Model model) {
 		List<Patient> list = patientService.getPatientList();
-		
+
 		model.addAttribute("patientList", list);
-		
+
 		return "/patient/patientList/patientList";
 	}
-	
+
+	// 환자 권한 리스트 검색
 	@PostMapping("/patientList")
-	public String getPatientList( @RequestParam(value="sk3") String sk3
-								,@RequestParam(value="sv3") String sv3
-								,Model model) {
+	public String getPatientList(@RequestParam(value = "sk3") String sk3, @RequestParam(value = "sv3") String sv3,
+			Model model) {
 		List<Patient> list = patientService.getPatientSearchList(sk3, sv3);
 		model.addAttribute("patientList", list);
-		
+
 		return "/patient/patientList/patientList";
 	}
-	
+
+	// 환자 권한 회원등록 폼
 	@GetMapping("/addPatient")
 	public String addPatient() {
 		return "/patient/addPatient/addPatient";
 	}
-	
+
+	// 환자 권한 회원등록 및 유효성 검사
 	@PostMapping("/addPatient")
 	public String addPatient(Patient patient, Model model) {
 		System.out.println(patient);
 		Patient patientCheck = patientService.getPatientById(patient.getP_id());
-		
-		if(patientCheck != null) {
+
+		if (patientCheck != null) {
 			model.addAttribute("result", "동일한 아이디가 존재합니다.");
 			return "/patient/addPatient/addPatient";
 		}
-		
+
 		patientService.addPatient(patient);
-		
+
 		return "redirect:/patientList";
 	}
-	
+
+	// 환자 권한 회원수정 폼
 	@GetMapping("/modifyPatient")
-	public String modifyPatient(@RequestParam(value="patientId")
-								String patientId, Model model) {
+	public String modifyPatient(@RequestParam(value = "patientId") String patientId, Model model) {
 		System.out.println(patientId + "<--patientId");
-		
+
 		model.addAttribute("patient", patientService.getPatientById(patientId));
-		
+
 		return "/patient/modifyPatient/modifyPatient";
 	}
-	
+
+	// 환자 권한 회원수정 및 유효성 검사
 	@PostMapping("/modifyPatient")
 	public String modifyPatient(Patient patient) {
 		patientService.modifyPatient(patient);
 		return "redirect:/patientList";
 	}
-	
+
+	// 환자 권한 삭제 폼
 	@GetMapping("/delPatient")
-	public String delPatient(@RequestParam(value="patientId")
-							String patientId, Model model) {
+	public String delPatient(@RequestParam(value = "patientId") String patientId, Model model) {
 		model.addAttribute("patientId", patientId);
-		
+
 		return "patient/delPatient/delPatient";
 	}
+
+	// 환자 권한 회원 삭제 및 유효성 검사
 	@PostMapping("/delPatient")
-	public String delPatient(Patient patient,Model model) {
-		
-		int result = patientService.deletePatient(patient.getP_id(), 
-				patient.getP_pw());
-		
-		if(result == 0) {
+	public String delPatient(Patient patient, Model model) {
+
+		int result = patientService.deletePatient(patient.getP_id(), patient.getP_pw());
+
+		if (result == 0) {
 			model.addAttribute("result", "비밀번호가 일치하지 않습니다.");
 			model.addAttribute("patientId", patient.getP_id());
 			return "patient/delPatient/delPatient";
 		}
-		
+
 		return "redirect:/patientList";
-	
-	
-}
+
+	}
 }
--- oneteam_mds/src/main/java/com/cafe24/oneteammds/service/AdminService.java
+++ oneteam_mds/src/main/java/com/cafe24/oneteammds/service/AdminService.java
@@ -1,4 +1,5 @@
 package com.cafe24.oneteammds.service;
+
 //계정 오류로 인한 push
 import java.util.HashMap;
 import java.util.List;
@@ -11,69 +12,73 @@ import org.springframework.transaction.annotation.Transactional;
 import com.cafe24.oneteammds.mapper.AdminMapper;
 import com.cafe24.oneteammds.vo.Admin;
 
-
-
 @Service
 @Transactional
 public class AdminService {
 
 	@Autowired
 	private AdminMapper adminMapper;
-	
+
+	// 관리자 권한 로그인
 	public Map<String, Object> getAdminLogin(Admin admin) {
-		
+
 		String result = "";
-		
-		Map<String, Object> map = new HashMap<String,Object>();
-		
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
 		Admin adminCheck = adminMapper.getAdminLogin(admin);
-		
-		if(adminCheck == null) {
-			Admin adminIdCheck = adminMapper.getAdminById(
-					admin.getA_id());
-			if(adminIdCheck == null) {
+
+		if (adminCheck == null) {
+			Admin adminIdCheck = adminMapper.getAdminById(admin.getA_id());
+			if (adminIdCheck == null) {
 				result = "등록된 아이디의 정보가 없습니다.";
-			}else {
+			} else {
 				result = "비밀번호가 일치하지 않습니다.";
 			}
 
-		}else {
+		} else {
 			result = "로그인 성공";
 			map.put("loginAdmin", adminCheck);
 		}
-		
+
 		map.put("result", result);
-		
+
 		return map;
 	}
-	
-	public List<Admin> getAdminList(){
+
+	// 관리자 권한 리스트
+	public List<Admin> getAdminList() {
 		List<Admin> list = adminMapper.getAdminList();
 		return list;
 	}
-	
-	public List<Admin> getAdminSearchList(String sk, String sv){
+
+	// 관리자 권한 리스트 검색
+	public List<Admin> getAdminSearchList(String sk, String sv) {
 		List<Admin> list = adminMapper.getAdminSearchList(sk, sv);
-		
+
 		return list;
 	}
-	
+
+	// 관리자 권한 회원등록
 	public int addAdmin(Admin admin) {
 		int result = adminMapper.addAdmin(admin);
 		return result;
 	}
-	
+
+	// 관리자 권한 로그인 값 가져오기
 	public Admin getAdminById(String a_id) {
-		
+
 		return adminMapper.getAdminById(a_id);
 	}
-	
+
+	// 관리자 회원정보 수정
 	public int modifyAdmin(Admin admin) {
 		return adminMapper.modifyAdmin(admin);
 	}
-	
+
+	// 관리자 권한 회원삭제
 	public int deleteAdmin(String a_id, String a_pw) {
 		return adminMapper.delAdmin(a_id, a_pw);
 	}
-	
+
 }
