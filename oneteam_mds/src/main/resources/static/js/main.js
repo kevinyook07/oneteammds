eabc72946592c1767ce086c14ad15d293728146d
yongsuLee <ud1746@naver.com>
2019-10-18 14:11:38 +0900

    이용수
    1. majn.js 엔터클릭시 채팅전송 함수 이벤트 추가
    
    2. SusinHospitalController get방식으로 sinHospitalList 설정
    
    3. sinHospitalList 진료정보 수신내역 임시 값 입력

--- oneteam_mds/src/main/resources/static/js/main.js
+++ oneteam_mds/src/main/resources/static/js/main.js
@@ -74,7 +74,7 @@ function onMessageReceived(payload) {
 
     if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
+        message.content = message.sender + '님 참여중';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' left!';
